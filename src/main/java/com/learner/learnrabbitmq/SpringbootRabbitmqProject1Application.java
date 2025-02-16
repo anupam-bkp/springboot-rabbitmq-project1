4913ab4fe931ded389a030d6ace2c495cd1bf722
anupam <anupam.nitdgp14@gmail.com>
2025-02-16 11:58:50 +0530

    Initial project setup from Spring initializer.

---
+++ src/main/java/com/learner/learnrabbitmq/SpringbootRabbitmqProject1Application.java
@@ -0,0 +1,13 @@
+package com.learner.learnrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqProject1Application.class, args);
+	}
+
+}
